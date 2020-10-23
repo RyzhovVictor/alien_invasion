d44ef290dd82ddbccc68ff5e734e0da73665186f
Виктор Рыжов <ryzhov.dur@gmail.com>
2020-10-23 22:43:27 +0300

    Alien Invasion worked fleet

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import pygame
 
 class Settings:
     def __init__(self):
+        # Настройка экрана
         self.screen_width = 1400
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
@@ -10,13 +11,35 @@ class Settings:
         # Настройки коробля
         self.ship_speed = 1.5
         self.ship_limit = 3
+
         # Параметры снаряда
         self.bullet_speed = 1.5
-        self.bullet_width = 5
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 7
-#       Настройки пришельцев
+        self.bullets_allowed = 3
+
+        # Настройки пришельцев
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
+
+        # Темп ускорения игры
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Иницилизирует настройки, изменяющиеся в ходе игры"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Увеличивает настройки скорости"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
